e4746a988d8b6c33161a669699de2a6c6cf5df60
Валерий Синицын <crucifer@live.ru>
2023-03-03 09:45:34 +0300

    feat: add args module, feat: add args mode usage in weather.js

--- weather.js
+++ weather.js
@@ -1,1 +1,24 @@
 #!/user/bin/env node
+
+import { getArgs } from './helpers/args.js'
+
+const initCLI = () => {
+    const args = getArgs(process.argv)
+    console.log(args)
+
+    if (args.h) {
+        //show help
+    }
+
+    if (args.s) {
+        //save city
+    }
+
+    if (args.t) {
+        //save token
+    }
+
+    //show weather
+}
+
+initCLI()
